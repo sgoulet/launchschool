b06c3f3a6445bbc5b5cebf028d519357f34bfd5f
Shawn Goulet <shawng@capecodcommission.org>
2015-12-29 17:00:57 -0500

    adding arrays exercises'solutions.

---
+++ Arrays/exercise-3.rb
@@ -0,0 +1,5 @@
+#How do you print the word "example" from the following array?
+
+#arr = [["test", "hello", "world"],["example", "mem"]]
+
+#You would have to execute an 'arr.last.first'.
---
+++ Arrays/exercise-4.rb
@@ -0,0 +1,9 @@
+# What does each method return in the following example?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+1. arr.index(5)
+
+2. arr.index[5]
+
+3. arr[5]
